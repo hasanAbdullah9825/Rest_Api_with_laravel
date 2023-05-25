e138b8b3656a35f065ff7055b419a2aa051a6507
Abdullah <hasan.techtomorrow@gmail.com>
2023-05-25 16:14:20 +0600

    Initial commit

---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Customer;
+use App\Models\Invoice;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    // public function run()
+    // {
+    //     Customer::factory()
+    //         ->count(7)
+    //         ->hasInvoices(2,function ( array $attributes,Customer $user) {
+    //             return ['id' => $user->id];
+    //         })
+    //         ->create();
+
+
+    // }
+
+    public function run()
+    {
+
+        Customer::factory()->count(25)
+            ->hasInvoices(10, function (array $attributes, Customer $customer) {
+                return [
+                    'customer_id' => $customer->id,
+                ];
+            })
+            ->create();
+
+
+
+            Customer::factory()->count(100)
+            ->hasInvoices(5, function (array $attributes, Customer $customer) {
+                return [
+                    'customer_id' => $customer->id,
+                ];
+            })
+            ->create();
+
+
+
+            Customer::factory()->count(100)
+            ->hasInvoices(3, function (array $attributes, Customer $customer) {
+                return [
+                    'customer_id' => $customer->id,
+                ];
+            })
+            ->create();
+
+
+
+            Customer::factory()->count(5)
+             ->create();
+    }
+}
