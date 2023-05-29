cca8ff473b6e84cdb517c6307d662cee24e44e09
Abdullah <hasan.techtomorrow@gmail.com>
2023-05-29 18:38:24 +0600

    Creating Resource with post Request .

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -6,7 +6,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
 use Illuminate\Http\Request;
-use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\V1\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
@@ -50,8 +50,8 @@ class CustomerController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(StoreCustomerRequest $request)
-    {
-        //
+    {  
+        return new CustomerResource(Customer::create($request->all()));
     }
 
     /**
